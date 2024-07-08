1de7f443c3a1cd3b53ff23ccec6d3b92a3613b01
prathiba1105 <prathibarp11@gmail.com>
2024-07-08 15:55:44 +0530

    test1

---
+++ ClientDetails.java
@@ -0,0 +1,11 @@
+package org.client;
+
+public class ClientDetails {
+	public void clName() {
+	 System.out.println("Enter client Name : Sundar");
+	}
+	public void clLocation() {
+		System.out.println("Enter client location : Bagmane");
+	}
+
+}
---
+++ Simple.java
@@ -0,0 +1,12 @@
+package org.client;
+
+public class Simple {
+	public static void main(String[] args) {
+		String s1 = null;
+		char c = s1.charAt(2);
+		System.out.println(c);
+		
+	}
+
+
+}
